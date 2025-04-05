bb9f042881f45fe4fbd14450b3b00b89cf8f3ddd
Adam Scott <ascott.ca@gmail.com>
2025-04-05 11:18:34 -0400

    Add WIP basic godot-rust library

---
+++ rust/build-wasm.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd `dirname "$0"`
+
+cargo +nightly build --package state_of_godot_and_the_web_2025_gdextension &&
+cargo +nightly build --package state_of_godot_and_the_web_2025_gdextension \
+	--features godot/experimental-wasm,godot/lazy-function-tables \
+	--target wasm32-unknown-emscripten -Zbuild-std $@
+
---
+++ rust/src/lib.rs
@@ -0,0 +1,6 @@
+use godot::prelude::*;
+
+struct StateOfTheWeb2025GDExtension;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for StateOfTheWeb2025GDExtension {}
