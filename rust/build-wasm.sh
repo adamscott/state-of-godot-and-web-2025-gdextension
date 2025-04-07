fd112e5a137a0329d2ae876d01631c4eeb0c6d29
Adam Scott <ascott.ca@gmail.com>
2025-04-07 17:39:35 -0400

    Add HelloRust node

--- rust/build-wasm.sh
+++ rust/build-wasm.sh
@@ -4,6 +4,6 @@ cd `dirname "$0"`
 
 cargo +nightly build --package state_of_godot_and_the_web_2025_gdextension &&
 cargo +nightly build --package state_of_godot_and_the_web_2025_gdextension \
-	--features godot/experimental-wasm,godot/lazy-function-tables \
+	--features godot/experimental-wasm,godot/experimental-wasm-nothreads,godot/lazy-function-tables \
 	--target wasm32-unknown-emscripten -Zbuild-std $@
 
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,20 @@
+use godot::classes::INode;
+use godot::classes::Node;
 use godot::prelude::*;
+
+#[derive(GodotClass)]
+#[class(base=Node)]
+struct HelloRust {
+    base: Base<Node>,
+}
+
+#[godot_api]
+impl INode for HelloRust {
+    fn init(base: Base<Node>) -> Self {
+        godot_print!("Hello, World!");
+        Self { base }
+    }
+}
 
 struct StateOfTheWeb2025GDExtension;
 
